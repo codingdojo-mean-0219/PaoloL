24ff4c816d5ecc4926558154e32fd823ac93e1a6
Paolo Leano <leanopaolo@gmail.com>
2019-03-30 18:50:24 -0700

    BooksAPI

---
+++ 05-angular/02-fetch-data/booksApi/public/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'public';
+  authors = [];
+  constructor(private _httpService: HttpService) {}
+
+  ngOnInit() {
+    this.getAuthorsFromService();
+  }
+  getAuthorsFromService() {
+    let observable = this._httpService.getAuthors();
+    observable.subscribe(data => {
+      console.log(data);
+      this.authors = data['docs'];
+    });
+  }
+}
---
+++ 05-angular/02-fetch-data/booksApi/public/src/app/http.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private _http: HttpClient) {}
+  getAuthors() {
+    return this._http.get('/authors');
+    // tempObserve.subscribe(data => console.log(data));
+  }
+}
