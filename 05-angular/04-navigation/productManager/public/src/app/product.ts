1f17d2569b090563acbdb51ee3df47df29859ba3
Paolo Leano <leanopaolo@gmail.com>
2019-04-05 09:49:13 -0700

    productManager

---
+++ 05-angular/04-navigation/productManager/public/src/app/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+  constructor(
+    public id: number = -1,
+    public name: string = '',
+    public price: number = 1,
+    public img: string = '../../assets/product.jpeg'
+  ) {}
+}
